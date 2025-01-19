0a9e3484d3d3dd20b83ba9d12547feacea98455f
Martyna Łoboda <martyna.loboda@outlook.com>
2025-01-19 20:22:15 +0100

    feature: brush drawing function added

--- Graphic-editor/MainWindow.xaml.cs
+++ Graphic-editor/MainWindow.xaml.cs
@@ -11,11 +11,61 @@ using System.Windows.Shapes;
 
 namespace Graphic_editor
 {
+    enum  DrawStyle
+    {
+        Freestyle
+    }
     public partial class MainWindow : Window
     {
+        private Point _currentMouseLocation = new();
+        private DrawStyle _drawStyle = DrawStyle.Freestyle;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Changing drawStyle
+        private void ButtonBrushClick(object sender, RoutedEventArgs e)
+        {
+            _drawStyle = DrawStyle.Freestyle;
+        }
+
+
+        //mouse event handling
+        private void paintingSurfaceMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && _drawStyle == DrawStyle.Freestyle)
+            {
+                Brush brushColor = new SolidColorBrush(Colors.Black);
+                Line line = new Line();
+                line.Stroke = brushColor;
+                line.X1 = _currentMouseLocation.X;
+                line.Y1 = _currentMouseLocation.Y;
+                line.X2 = e.GetPosition(paintingSurface).X; 
+                line.Y2 = e.GetPosition(paintingSurface).Y;
+
+                _currentMouseLocation = e.GetPosition(paintingSurface);
+
+                paintingSurface.Children.Add(line);
+            }
+            else
+                _currentMouseLocation = e.GetPosition(paintingSurface);
+        }
+
+        private void paintingSurfaceMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void paintingSurfaceMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                _currentMouseLocation = e.GetPosition(paintingSurface);
+                e.GetPosition(paintingSurface);
+            }
+
         }
     }
 }
