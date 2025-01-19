e3946e2010ff839975dd2793f492cba84b8f35a9
Martyna Łoboda <martyna.loboda@outlook.com>
2025-01-19 22:19:13 +0100

    feature: straight line drawing function added

--- Graphic-editor/MainWindow.xaml.cs
+++ Graphic-editor/MainWindow.xaml.cs
@@ -1,24 +1,21 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Graphic_editor;
 
-enum  DrawStyle
+enum DrawStyle
 {
     Freestyle,
-    Point
+    Point,
+    StraightLine
 }
 public partial class MainWindow : Window
 {
-    private Point _currentMouseLocation = new();
+    private Point _currentMouseLocation;
+    private Point? _startMouseLocation = null;
     private DrawStyle _drawStyle = DrawStyle.Freestyle;
 
     public MainWindow()
@@ -30,52 +27,63 @@ public partial class MainWindow : Window
     private void ButtonBrushClick(object sender, RoutedEventArgs e)
     {
         _drawStyle = DrawStyle.Freestyle;
+        _startMouseLocation = null;
     }
     private void ButtonPointClick(object sender, RoutedEventArgs e)
     {
         _drawStyle = DrawStyle.Point;
+        _startMouseLocation = null;
+    }
+    private void ButtonStraightLineClick(object sender, RoutedEventArgs e)
+    {
+        _drawStyle = DrawStyle.StraightLine;
     }
 
 
     // --- Handling mouse events ---
-    private void paintingSurfaceMouseMove(object sender, MouseEventArgs e)
+    private void PaintingSurfaceMouseMove(object sender, MouseEventArgs e)
     {
         if (e.LeftButton == MouseButtonState.Pressed && _drawStyle == DrawStyle.Freestyle)
         {
             Brush brushColor = new SolidColorBrush(Colors.Black);
-            Line line = new Line();
-            line.Stroke = brushColor;
-            line.X1 = _currentMouseLocation.X;
-            line.Y1 = _currentMouseLocation.Y;
-            line.X2 = e.GetPosition(paintingSurface).X; 
-            line.Y2 = e.GetPosition(paintingSurface).Y;
+            Line line = new()
+            {
+                Stroke = brushColor,
+                X1 = _currentMouseLocation.X,
+                Y1 = _currentMouseLocation.Y,
+                X2 = e.GetPosition(PaintingSurface).X,
+                Y2 = e.GetPosition(PaintingSurface).Y
+            };
 
-            _currentMouseLocation = e.GetPosition(paintingSurface);
+            _currentMouseLocation = e.GetPosition(PaintingSurface);
 
-            paintingSurface.Children.Add(line);
+            PaintingSurface.Children.Add(line);
         }
         else
-            _currentMouseLocation = e.GetPosition(paintingSurface);
+            _currentMouseLocation = e.GetPosition(PaintingSurface);
     }
 
-    private void paintingSurfaceMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    private void PaintingSurfaceMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        _currentMouseLocation = e.GetPosition(paintingSurface);
-        switch(_drawStyle)
+        _currentMouseLocation = e.GetPosition(PaintingSurface);
+        switch (_drawStyle)
         {
             case DrawStyle.Point:
                 AddPoint();
+                break;
+            case DrawStyle.StraightLine:
+                AddStraightLine();
                 break;
 
         }
     }
 
-    private void paintingSurfaceMouseDown(object sender, MouseButtonEventArgs e)
+    private void PaintingSurfaceMouseDown(object sender, MouseButtonEventArgs e)
     {
         if (e.ButtonState == MouseButtonState.Pressed)
         {
-            _currentMouseLocation = e.GetPosition(paintingSurface);
-            e.GetPosition(paintingSurface);
+            _currentMouseLocation = e.GetPosition(PaintingSurface);
+            e.GetPosition(PaintingSurface);
         }
 
     }
@@ -84,9 +92,11 @@ public partial class MainWindow : Window
     // --- Drawing methods ---
     private void AddPoint()
     {
-        Ellipse elipse = new Ellipse();
-        elipse.Width = 6;
-        elipse.Height = 6;
+        Ellipse elipse = new()
+        {
+            Width = 6,
+            Height = 6
+        };
 
         Canvas.SetTop(elipse, _currentMouseLocation.Y - elipse.Height / 2);
         Canvas.SetLeft(elipse, _currentMouseLocation.X - elipse.Width / 2);
@@ -94,8 +104,30 @@ public partial class MainWindow : Window
         Brush brushColor = new SolidColorBrush(Colors.Black);
         elipse.Fill = brushColor;
 
-        paintingSurface.Children.Add(elipse);
+        PaintingSurface.Children.Add(elipse);
     }
 
+    private void AddStraightLine()
+    {
+        if (_startMouseLocation == null)
+        {
+            _startMouseLocation = _currentMouseLocation;
+        }
+        else
+        {
+            Brush brushColor = new SolidColorBrush(Colors.Black);
+            Line line = new()
+            {
+                Stroke = brushColor,
+                X1 = _startMouseLocation.Value.X,
+                Y1 = _startMouseLocation.Value.Y,
+                X2 = _currentMouseLocation.X,
+                Y2 = _currentMouseLocation.Y
+            };
 
+            PaintingSurface.Children.Add(line);
+
+            _startMouseLocation = null;
+        }
+    }
 }
